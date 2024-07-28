288c9548d5759b5d42b5b7b7c5fe51bbc17a4832
yeabalex <yeabalex18@gmail.com>
2024-07-28 01:44:46 -0700

    proxy

--- src/index.ts
+++ src/index.ts
@@ -33,12 +33,12 @@ app.use(cors({
 
 app.use(express.json())
 app.use(session({
-    name: 'connect.sid',
     secret: 'iamyeabsira',
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 60000*60, sameSite: 'lax', httpOnly: true, secure: true}
+    cookie: { maxAge: 60000*60, sameSite: 'none', httpOnly: true}
   }));
+app.set("trust proxy", 1);
 
 app.use(passport.initialize())
 app.use(passport.session())
